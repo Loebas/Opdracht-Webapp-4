41d2eca7c3f08f4b2965d3735c109bb6c4fa01de
WouterMestdagh <mestdagh.wo@gmail.com>
2019-05-15 21:57:17 +0200

    game model

---
+++ gameapp/src/app/game.ts
@@ -0,0 +1,14 @@
+export class Game {
+    constructor(
+        private _id: number,
+        private _naam: string,
+        private _minSpelers: number,
+        private _maxSpelers: number,
+        private _difficulty: number,
+        private _imageUrl: string
+    ) {
+
+    }
+
+
+}
